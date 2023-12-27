bdd5622552ce5fd707d77b7a520a5e4154ae79f6
Dracowyn <dracowyn@outlook.com>
2023-12-27 15:56:16 +0800

    评论多图上传接口

--- application/shop/controller/Product.php
+++ application/shop/controller/Product.php
@@ -11,6 +11,14 @@ use think\response\Json;
 
 class Product extends Controller
 {
+	protected $businessModel = null;
+
+	public function _initialize()
+	{
+		parent::_initialize();
+		$this->businessModel = new \app\common\model\business\Business;
+	}
+
 	public function thumb(): string
 	{
 		$proId = $this->request->param('proid', 0, 'trim');
@@ -24,5 +32,20 @@ class Product extends Controller
 		$proId = $this->request->param('proid', 0, 'trim');
 		$product = model('product.Product')->where(['id' => $proId])->find();
 		return json($product['thumbs_cdn']);
+	}
+
+	// 评论图片上传接口
+	public function upload(): Json
+	{
+		$id = $this->request->param('id', 0, 'trim');
+		$business = $this->businessModel->find($id);
+		if (!$business) {
+			return json(['msg' => '用户不存在', 'code' => 0, 'data' => null]);
+		}
+
+		$images = build_upload('image');
+
+//		$images = build_upload('images');
+		return json($images);
 	}
 }
